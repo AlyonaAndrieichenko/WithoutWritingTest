93fee116140b18f23d0e7442e058dc060fed2274
ElenaK <149805926+ElenaKas8@users.noreply.github.com>
2024-04-24 10:14:08 +0200

    task updated2

--- OlenaKaspruk/task1.js
+++ OlenaKaspruk/task1.js
@@ -1,6 +1,6 @@
 //Задача. Вывести названия трех произвольных треков с продолжительностью от 5 до 60 мин (вкл.)
-
 db.tracks.aggregate([
     { $match: { duration_secs: { $gte: 60 * 5, $lte: 60 * 60 } } },
-    { $sample: { size: 3 } }
+    { $sample: { size: 3 } },
+    { $project: { _id: 0, name: 1 } }
 ])
